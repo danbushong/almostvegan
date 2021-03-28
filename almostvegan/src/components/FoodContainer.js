6c21608c8fc65647de6fb5cfa0132eaa56d87b99
Dan Bushong <bushongdan@gmail.com>
2021-03-28 10:30:28 -0500

    working with query data

--- almostvegan/src/components/FoodContainer.js
+++ almostvegan/src/components/FoodContainer.js
@@ -1,31 +1,40 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { useAtom } from "jotai";
+import { wasItSubmitted} from "../Atoms"
 
 
 function FoodContainer() {
 
   const [query, setQuery] = useState([]);
 
-  const [recipeList, setRecipeList] = useState([])
+  const [recipeReturned, setRecipeList] = useState([])
+
+  const [didItSubmit, setWasItSubmitted] = useAtom(wasItSubmitted)
+
+  
+
+  
+  
 
   const APP_ID = "171a1274";
 
   const APP_KEY = "2f55324f23c054712335c6346d529523"
 
-  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&health=dairy-free`
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&health=dairy-free&health=egg-free`
 
   useEffect(()=> {
     loadRecipes()
   }, [])
 
   
-
+ 
 
   function loadRecipes(){
 
     axios.get(url).then((res) => {
       setRecipeList(res.data.hits[0])
-      console.log(res.data.hits[0])
+      
     })
 
     
@@ -33,23 +42,15 @@ function FoodContainer() {
 
 
   };
-  
-  
-
-
-  
-
-
-  
-    
-    
 
     
 
   const onSubmit = (e) => {
     e.preventDefault();
     
-    loadRecipes()
+    setWasItSubmitted(true)
+    
+    console.log(recipeReturned)
 
 
 
@@ -58,6 +59,8 @@ function FoodContainer() {
   const onChange = e => {
     setQuery(e.target.value)
   }
+
+  
 
 
 
@@ -68,10 +71,20 @@ function FoodContainer() {
         <input type="text" placeholder="Search" onChange={onChange} value={query} />
         <input type="submit" value="search" />
       </form>
-      {recipeList.length ? (
-        <div>{recipeList}</div>
+      {wasItSubmitted !== false ? (
+        <div>what ya wanna eat?</div>
+        
+        
+        
+      ) : (
+        
+        
+        <img src={recipeReturned.recipe.image}></img>
+      )}
+      
+    
 
-      ): (null)}
+      
       
     </div>
   )
